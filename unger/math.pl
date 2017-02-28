9abc26b62c8eb2d37be531a26e05eb1e19a6564a
Carl Olson <carlolson@users.noreply.github.com>
2017-02-28 11:18:13 -0600

    complete ungar parser for simple arithmetic

--- unger/math.pl
+++ unger/math.pl
@@ -1,6 +1,50 @@
 
 :- use_module(library(clpfd)).
 
+%% Grammar from page 105
+% Expr_s -> Expr + Term | Term
+% Term   -> Term x Factor | Factor
+% Factor -> ( Expr ) | i
+
+%% [
+%%     expr   : [[expr, '+', term], [term]],
+%%     term   : [[term, 'x', factor], [factor]],
+%%     factor : [['(', expr, ')'], ['i']]
+%% ]
+
+parse(Input, Tree) :-
+    string_chars(Input, Chars),
+    delete(Chars, ' ', CharsNoSpaces),
+    parse_expr(CharsNoSpaces, Tree).
+
+parse_expr(Input, expr(Left, '+', Right)) :-
+    partition(3, Input, Output),
+    Output = [Expr, ['+'], Term],
+    parse_expr(Expr, Left),
+    parse_term(Term, Right).
+parse_expr(Input, expr(Tree)) :-
+    partition(1, Input, Output),
+    Output = [Term],
+    parse_term(Term, Tree).
+
+parse_term(Input, term(Left, 'x', Right)) :-
+    partition(3, Input, Output),
+    Output = [Term, ['x'], Factor],
+    parse_term(Term, Left),
+    parse_factor(Factor, Right).
+parse_term(Input, term(Tree)) :-
+    partition(1, Input, Output),
+    Output = [Factor],
+    parse_factor(Factor, Tree).
+
+parse_factor(Input, factor('(', Tree, ')')) :-
+    partition(3, Input, Output),
+    Output = [['('], Expr, [')']],
+    parse_expr(Expr, Tree).
+parse_factor(Input, factor('i')) :-
+    partition(1, Input, Output),
+    Output = [['i']].
+
 take(0, Rest, []:Rest).
 take(N0, [A | As], [A | Bs]:Rest) :-
     N #= N0 - 1,
@@ -15,7 +59,7 @@ partition(1, Input, [Input]) :-
     is_list(Input),
     Input \== [].
 
-:- begin_tests(arithmetic).
+:- begin_tests(partition).
 
 test("partition fails when given []",
      fail) :-
@@ -42,4 +86,4 @@ test("partition produces multiple solutions",
 		     [[1, 2, 3], [4]] ])) :-
     partition(2, [1, 2, 3, 4], Output).
 
-:- end_tests(arithmetic).
+:- end_tests(partition).
